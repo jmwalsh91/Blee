70086c7771d931bd4df8d3843d4a54ca28525b4d
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-15 23:59:51 -0600

    task: nativebase provider

--- App.js
+++ App.js
@@ -1,20 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { NativeBaseProvider, Box, View } from "native-base";
+import { Text } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
+    <NativeBaseProvider>
+   
+       
+        <Box alignSelf="center" bg="primary.500" _text={{
+      fontSize: "md",
+      fontWeight: "medium",
+      color: "warmGray.50",
+        }}>Hello</Box>
+    
+   
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
