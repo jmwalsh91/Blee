a9c667bb1041be1636a21e96bbd93faa30645e13
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-20 22:50:36 -0600

    task: chromaFn scaleBetween + scaleBetweenNormalized

--- utils/chromaFn.js
+++ utils/chromaFn.js
@@ -2,3 +2,9 @@ export function scaleBetween (startColor, endColor) {
     return chroma.scale([startColor, endColor])
     .mode('lch').colors(6)
 }
+
+export function scaleBetweenNormalized (scaleArray) {
+    return chroma.scale(scaleArray)
+    .mode('lch').colors(6).correctLightness()
+    
+}
