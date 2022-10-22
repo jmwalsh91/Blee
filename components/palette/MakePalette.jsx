45a98c660b0b367acfb1341231bbafd57444eec2
jmwalsh91 <jordanmwalsh@gmail.com>
2022-10-21 23:42:41 -0600

    task: MakePallete functionality

--- components/palette/MakePalette.jsx
+++ components/palette/MakePalette.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {ZStack, Box} from 'native-base';
 import { ColorPicker } from 'react-native-color-picker'
 /**
  * TODO: Determine ideal UI for color picker, and pass values to scaleBetween
  */
 function MakePalette() {
+  const [startColor, setStartColor] = React.useState('#000000')
+  const [endColor, setEndColor] = React.useState('#FFFFFF')
+  const [collarArray, setCollarArray] = React.useState([]);
+
+useEffect(() => {
+   const colorArray = scaleBetween(startColor, endColor, 10);
+     setCollarArray(colorArray);
+  }, [startColor, endColor]);
+
+
   return (
     <>
     <ColorPicker
-       onColorSelected={color => alert(`Selected: ${color}`)}
+       onColorSelected={color => setStartColor(color)}
        style={styles.colorContainer}
     />
     <ColorPicker
-       onColorSelected={color => alert(`Selected: ${color}`)}
+       onColorSelected={color => setEndColor(color)}
        style={styles.colorContainer}
     />
     <ZStack mt="40" ml={-50}>
-          <Box bg="success.700" size="20" rounded="lg" shadow={3} />
-          <Box bg="success.500" mt="5" ml="5" size="20" rounded="lg" shadow={5} />
-          <Box bg="success.300" mt="10" ml="10" size="20" rounded="lg" shadow={7} />
+      {collarArray.map((color, index) => (
+        <Box key={index} bg={`${color}`} size="20" rounded="lg" shadow={3} />
+      ))}
         </ZStack>
     </>
   )
